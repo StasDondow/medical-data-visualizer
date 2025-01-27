08e61764e5d8a89d80762944fe49b3fc7153ce70
Stanislav Dondov <dondovwork@gmail.com>
2025-01-27 14:59:14 +0000

    project 3 done

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,50 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+# Import the data from medical_examination.csv and assign it to the df variable.
+df = pd.read_csv('medical_examination.csv')
 
-# 2
-df['overweight'] = None
+# Add an overweight column to the data. 
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
 
-# 3
+# Normalize data by making 0 always good and 1 always bad. If the value of cholesterol or gluc is 1, set the value to 0. If the value is more than 1, set the value to 1.
+df[['gluc','cholesterol']] = (df[['gluc','cholesterol']] > 1).astype(int)
 
-
-# 4
+# Draw the Categorical Plot in the draw_cat_plot function.
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # Create a DataFrame for the cat plot using pd.melt with values from cholesterol, gluc, smoke, alco, active, and overweight in the df_cat variable.
+    # Group and reformat the data in df_cat to split it by cardio. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
+    # Convert the data into long format and create a chart that shows the value counts of the categorical features using the following method provided by the seaborn library
+    fig = sns.catplot(data = df_cat, kind='count',  x='variable', hue='value', col='cardio').set(ylabel = 'total').fig
 
-    # 6
-    df_cat = None
-    
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
     fig.savefig('catplot.png')
     return fig
 
 
-# 10
+# Draw the Heat Map in the draw_heat_map function.
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Clean the data in the df_heat variable by filtering out the following patient segments that represent incorrect data
+    df_heat = df[ 
+        ( df['ap_lo'] <= df['ap_hi'] ) & 
+        ( df['height'] >= df['height'].quantile(0.025) ) & 
+        ( df['height'] <= df['height'].quantile(0.975) ) & 
+        ( df['weight'] >= df['weight'].quantile(0.025) ) & 
+        ( df['weight'] <= df['weight'].quantile(0.975) ) 
+    ]
 
-    # 12
-    corr = None
+    # Calculate the correlation matrix and store it in the corr variable.
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
+    # Generate a mask for the upper triangle and store it in the mask variable.
+    mask = np.triu(corr)
 
+    # Set up the matplotlib figure.
+    fig, ax = plt.subplots()
 
+    # Plot the correlation matrix using the method provided by the seaborn library
+    ax = sns.heatmap(corr, mask=mask, annot=True, fmt='0.1f', square=True)
 
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
     fig.savefig('heatmap.png')
     return fig
